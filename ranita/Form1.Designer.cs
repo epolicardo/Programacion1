ecac5f47b3ff43674feff13fec891afc8386ee4c
Emiliano Policardo <emilianopolicardo@gmail.com>
2019-06-28 01:19:34 -0300

    Ranita

---
+++ ranita/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace ranita
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblFucsia = new System.Windows.Forms.Label();
+            this.lblRana = new System.Windows.Forms.Label();
+            this.lblLila = new System.Windows.Forms.Label();
+            this.lblAmarillo = new System.Windows.Forms.Label();
+            this.lblVerde = new System.Windows.Forms.Label();
+            this.lblAzul = new System.Windows.Forms.Label();
+            this.lblRojo = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblFucsia);
+            this.panel1.Controls.Add(this.lblRana);
+            this.panel1.Controls.Add(this.lblLila);
+            this.panel1.Controls.Add(this.lblAmarillo);
+            this.panel1.Controls.Add(this.lblVerde);
+            this.panel1.Controls.Add(this.lblAzul);
+            this.panel1.Controls.Add(this.lblRojo);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1331, 783);
+            this.panel1.TabIndex = 5;
+            // 
+            // lblFucsia
+            // 
+            this.lblFucsia.BackColor = System.Drawing.Color.Fuchsia;
+            this.lblFucsia.Location = new System.Drawing.Point(0, 600);
+            this.lblFucsia.Name = "lblFucsia";
+            this.lblFucsia.Size = new System.Drawing.Size(155, 28);
+            this.lblFucsia.TabIndex = 11;
+            // 
+            // lblRana
+            // 
+            this.lblRana.BackColor = System.Drawing.Color.Black;
+            this.lblRana.Location = new System.Drawing.Point(588, 700);
+            this.lblRana.Name = "lblRana";
+            this.lblRana.Size = new System.Drawing.Size(155, 28);
+            this.lblRana.TabIndex = 10;
+            // 
+            // lblLila
+            // 
+            this.lblLila.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.lblLila.Location = new System.Drawing.Point(0, 100);
+            this.lblLila.Name = "lblLila";
+            this.lblLila.Size = new System.Drawing.Size(155, 28);
+            this.lblLila.TabIndex = 9;
+            // 
+            // lblAmarillo
+            // 
+            this.lblAmarillo.BackColor = System.Drawing.Color.Yellow;
+            this.lblAmarillo.Location = new System.Drawing.Point(0, 200);
+            this.lblAmarillo.Name = "lblAmarillo";
+            this.lblAmarillo.Size = new System.Drawing.Size(155, 28);
+            this.lblAmarillo.TabIndex = 8;
+            // 
+            // lblVerde
+            // 
+            this.lblVerde.BackColor = System.Drawing.Color.Lime;
+            this.lblVerde.Location = new System.Drawing.Point(0, 300);
+            this.lblVerde.Name = "lblVerde";
+            this.lblVerde.Size = new System.Drawing.Size(155, 28);
+            this.lblVerde.TabIndex = 7;
+            // 
+            // lblAzul
+            // 
+            this.lblAzul.BackColor = System.Drawing.Color.Blue;
+            this.lblAzul.Location = new System.Drawing.Point(0, 400);
+            this.lblAzul.Name = "lblAzul";
+            this.lblAzul.Size = new System.Drawing.Size(155, 28);
+            this.lblAzul.TabIndex = 6;
+            // 
+            // lblRojo
+            // 
+            this.lblRojo.BackColor = System.Drawing.Color.Red;
+            this.lblRojo.Location = new System.Drawing.Point(0, 500);
+            this.lblRojo.Name = "lblRojo";
+            this.lblRojo.Size = new System.Drawing.Size(155, 28);
+            this.lblRojo.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1355, 807);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblFucsia;
+        private System.Windows.Forms.Label lblRana;
+        private System.Windows.Forms.Label lblLila;
+        private System.Windows.Forms.Label lblAmarillo;
+        private System.Windows.Forms.Label lblVerde;
+        private System.Windows.Forms.Label lblAzul;
+        private System.Windows.Forms.Label lblRojo;
+    }
+}
+
---
+++ ranita/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ranita
+{
+    public partial class Form1 : Form
+    {
+        Timer reloj = new Timer();
+        private int movimientoH = 10;
+        private int movimientoV = 1;
+        private int intervalo = 10;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode.ToString())
+            {
+                case "Left":
+                    lblRana.Left = lblRana.Left - 50;
+                    break;
+                case "Right":
+                    lblRana.Left = lblRana.Left + 50;
+                    break;
+                case "Up":
+                    lblRana.Top = lblRana.Top - 50;
+                    break;
+                case "Down":
+                    lblRana.Top = lblRana.Top + 50;
+                    break;
+            }
+        }
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            reloj.Interval = intervalo;
+            reloj.Start();
+            reloj.Tick += new EventHandler(moverAutos);
+        }
+
+        private void moverAutos(object sender, EventArgs e)
+        {
+            if (lblAzul.Left > this.Width)
+            {
+                lblAzul.Left = 0;
+            }
+            if (lblVerde.Left > this.Width)
+            {
+                lblVerde.Left = 0;
+            }
+            if (lblRojo.Left > this.Width)
+            {
+                lblRojo.Left = 0;
+            }
+            if (lblLila.Left > this.Width)
+            {
+                lblLila.Left = 0;
+            }
+            if (lblAmarillo.Left > this.Width)
+            {
+                lblAmarillo.Left = 0;
+            }
+            if (lblFucsia.Left > this.Width)
+            {
+                lblFucsia.Left = 0;
+            }
+            lblAzul.Left = lblAzul.Left + movimientoH + 4;
+            lblAmarillo.Left = lblAmarillo.Left + movimientoH + 3;
+            lblRojo.Left = lblRojo.Left + movimientoH - 2;
+            lblLila.Left = lblLila.Left + movimientoH - 1;
+            lblVerde.Left = lblVerde.Left + movimientoH - 1;
+            lblFucsia.Left = lblFucsia.Left + movimientoH;
+        }
+    }
+}
